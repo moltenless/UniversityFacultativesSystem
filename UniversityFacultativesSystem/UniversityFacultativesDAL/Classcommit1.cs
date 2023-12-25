c73cbc81666d23b6af70eb9d212ad8776d4ae9e0
moltenless <moltenless@gmail.com>
2023-12-25 17:28:05 +0200

    class commit 1

---
+++ UniversityFacultativesSystem/UniversityFacultativesDAL/Classcommit1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityFacultativesDAL
+{
+    internal class Classcommit1
+    {
+    }
+}

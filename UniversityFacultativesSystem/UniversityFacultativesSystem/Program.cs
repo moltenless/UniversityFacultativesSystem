e6707ce5bf6d4086c0d99ce1740cd82104202b3e
moltenless <moltenless@gmail.com>
2023-12-27 12:22:41 +0200

    adding forms switching functionality

---
+++ UniversityFacultativesSystem/UniversityFacultativesSystem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniversityFacultativesSystem
+{
+    internal static class Program
+    {
+        internal static FormSwitcher FormSwitcher { get; private set; }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            FormSwitcher = new FormSwitcher(new Form1());
+            Application.Run(FormSwitcher.ApplicationContext);
+        }
+    }
+}

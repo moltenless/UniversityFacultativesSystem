0b29e445ed7bf1640879ef14d496fbc7c3aaf711
moltenless <moltenless@gmail.com>
2023-12-27 19:57:12 +0200

    restyled start form

--- UniversityFacultativesSystem/UniversityFacultativesSystem/StartForm.cs
+++ UniversityFacultativesSystem/UniversityFacultativesSystem/StartForm.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 using UniversityFacultativesSystem.Properties;
 
@@ -11,10 +12,24 @@ namespace UniversityFacultativesSystem
             InitializeComponent();
             this.Style(3.6f, Color.LightSkyBlue);
             Icon = Resources.education_computer_school_graduate_cap_monitor_icon_149680;
-            button1.Style(3.6f, Color.LightSkyBlue);
-            button2.Style(3.6f, Color.LightSkyBlue);
-            button3.Style(1.6f, Color.LightSkyBlue);
-            button4.Style(1.6f, Color.LightSkyBlue);
+            deaneryButton.Style(2f, Color.LightSkyBlue);
+            teacherButton.Style(2f, Color.LightSkyBlue);
+            studentButton.Style(2f, Color.LightSkyBlue);
+            closeButton.Style(1.6f, Color.LightSkyBlue);
+            minimizeButton.Style(1.6f, Color.LightSkyBlue);
         }
+
+        private void RoleClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Close(object sender, EventArgs e) => this.Close();
+        private void Minimize(object sender, EventArgs e) => WindowState = FormWindowState.Minimized;
+        private void MouseEnteredRoles(object sender, EventArgs e)
+         => Controls.Find($"label{(int.Parse((sender as Control).Tag.ToString())) + 2}", false)[0].Visible = true;
+
+        private void MouseLeftRoles(object sender, EventArgs e)
+         => Controls.Find($"label{(int.Parse((sender as Control).Tag.ToString())) + 2}", false)[0].Visible = false;
     }
 }
